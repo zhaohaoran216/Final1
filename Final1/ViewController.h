876df192b5d2bc0263e492d452d05c1ae4d5763b
张悦心 <zhangyueshin@zhangyueshintekiMacBook-Air.local>
2015-07-07 10:50:49 +0800

    Initial Commit

---
+++ Final1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Final1
+//
+//  Created by 张悦心 on 15/7/7.
+//  Copyright (c) 2015年 ZHR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Final1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Final1
+//
+//  Created by 张悦心 on 15/7/7.
+//  Copyright (c) 2015年 ZHR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
